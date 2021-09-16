36893254a67712a1d3f8d0ad7cfc3aa33a361990
Geandry Rodriguez <geandryrodriguez@148-100-169-63.foxnet.marist.edu>
2021-09-16 19:04:16 -0400

    This program gives you pizza per square inch based on price and diameter

---
+++ PPI.py
@@ -0,0 +1,16 @@
+#Pie.persquareinch.py
+#this program calculates the price per square inch of a pizza, given the diameter and price of the pizza.
+
+import math
+
+def main():
+    print("this program calculates the price per square inch of a pizza")
+    print()
+    diameter = float(input("Enter the diameter of the pizza: "))
+    Area = float(math.pi * (diameter/2)**2)
+    price = float(input("Enter the price: "))
+    square = Area/price
+
+    print("the price per square inch of the pizza is" , square)
+
+main()
