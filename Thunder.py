e58f53a9b3beac7e5c27a820d0871bc7895c0843
Geandry Rodriguez <geandryrodriguez@148-100-169-63.foxnet.marist.edu>
2021-09-16 19:04:58 -0400

    This program calcultes the distance thunder struck

---
+++ Thunder.py
@@ -0,0 +1,13 @@
+#Thunder.py
+#This program calculates the distance of a lightning strike based on the time elasped betweem the flash and sound of thunder.
+
+import math
+def main():
+    print("this program calculates the distance of lighting strikes")
+    print()
+    time = float(input("Enter time elasped from when you saw the flash and then heard the sound"))
+    distance = time/5
+    
+    print("the distance  of the lightstrikes is:", distance, "miles")
+
+main()
