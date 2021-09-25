67d06b9540f274b3a4d17968d53abee5f4c82251
Geandry Rodriguez <geandryrodriguez@Geandrys-MacBook-Air.local>
2021-09-25 11:55:08 -0400

    This program returns the first and last characters of a set string

---
+++ return.py
@@ -0,0 +1,11 @@
+#firstandlast.py
+#This program takes a string and returns the first 2 char and last 2 char if the string is atleast greater than 2 char.
+
+def string_beg_ends(str):
+    if len(str) < 2:
+        return ''
+        
+    return str[0:2] + str[-2:]
+print(string_beg_ends('Jump'))
+print(string_beg_ends('ju'))
+print(string_beg_ends('j'))
