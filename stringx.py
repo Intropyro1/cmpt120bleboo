42b01ff658c5d85f015548773954fbf1ca381a42
Intropyro1 <Alan.Bleboo1@marist.edu>
2021-10-02 18:13:10 -0400

    This program returns the numeric of a given string

---
+++ stringx.py
@@ -0,0 +1,18 @@
+#Stringtonumbers.py
+#This program turns a given string into numbers
+
+
+
+
+#initializing toNumbers function
+def toNumbers(Fortnitesucks):
+    #looping each item in strList
+    for i in range(len(Fortnitesucks)):
+        #converting each string to an integer
+        strList[i]=int(strList[i])
+#initializing a list of strings
+strList=['0','1','2','3','4','5','6','7','8','9','10','11','12']
+#calling toNumbers function and passing strList
+toNumbers(strList)
+#printing the modified strList
+print(strList)
