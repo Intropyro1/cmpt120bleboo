eecc46d2c212f537b11dc8e1f16e5737783e5073
Juan Arias <juan.arias@marist.edu>
2021-10-03 15:14:21 -0400

    Feedback

--- PPI.py
+++ PPI.py
@@ -7,9 +7,9 @@ def main():
     print("this program calculates the price per square inch of a pizza")
     print()
     diameter = float(input("Enter the diameter of the pizza: "))
-    Area = float(math.pi * (diameter/2)**2)
+    area = float(math.pi * (diameter/2)**2)
     price = float(input("Enter the price: "))
-    square = Area/price
+    square = area/price
 
     print("the price per square inch of the pizza is" , square)
 
--- Thunder.py
+++ Thunder.py
@@ -5,7 +5,7 @@ import math
 def main():
     print("this program calculates the distance of lighting strikes")
     print()
-    time = float(input("Enter time elasped from when you saw the flash and then heard the sound"))
+    time = float(input("Enter time elasped from when you saw the flash and then heard the sound: "))
     distance = time/5
     
     print("the distance  of the lightstrikes is:", distance, "miles")
