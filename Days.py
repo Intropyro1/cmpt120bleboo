ac522fa2c687ef63ba178d2e2ccc8fd5ad537f77
Intropyro1 <Alan.Bleboo1@marist.edu>
2021-10-08 22:00:03 -0400

    This is week 6 homework assignment 2

---
+++ Days.py
@@ -0,0 +1,27 @@
+#Days
+#   This program gives days of the year are often numbered 1-366
+#Al
+
+
+
+import math
+
+def main():
+
+    dayNum = float(31(month - 1)+  day)
+    month = ["jan,feb,mar,apr,may,jun,jul,aug,sep,octo,nov,decm"]
+    boon = eval(input("enter your year:  "))
+
+    if month >= "feb" and year == boon:
+       newmonth = month - (4(month)+23)//10
+    return print(newmonth,"month")
+    if year % 4 == 0 and year % 100 == 0:
+        print("This is a leap year")
+    if feb (month = 29) == True:
+        print (month + 1)
+    elif year % 400 != 0:
+         print(year,"This is not a leap year")
+    else:
+        print(year,"This year is not a leap year")
+
+    return year
